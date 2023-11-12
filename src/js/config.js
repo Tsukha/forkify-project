e8d9355f72a0a7a80f59f392d3b83ad4395e3cce
tsukha <sandrotsukhishvili@gmail.com>
2023-11-12 17:49:55 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const RES_PER_PAGE = 10;
+export const KEY = 'a5714a33-0899-4d4e-8f5a-9cc9560289bf';
+export const MODAL_CLOSE_SEC = 0.25;
